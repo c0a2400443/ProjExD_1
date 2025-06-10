b85cf487b92e416397433f5eefb34820a091239e
c0a2400443 <c0a2400443@edu.teu.ac.jp>
2025-06-10 15:43:09 +0900

    練習６終了

--- pygame_image.py
+++ pygame_image.py
@@ -13,11 +13,14 @@ def main():
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img,True,False)
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
+        x=tmr
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img,[-x+1600,0])
         screen.blit(kt_img,[300,200])
         pg.display.update()
         tmr += 1        
